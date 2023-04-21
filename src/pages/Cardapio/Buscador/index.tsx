99edab533be4778e334e8cf2d2e0e2192e9b7395
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-04-21 14:48:40 -0300

    ...

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo, useMemo } from "react";
 import styles from "./Buscador.module.scss";
 import { CgSearch } from "react-icons/cg";
 
@@ -7,18 +7,23 @@ interface BuscadorProps {
     setBusca: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function Buscador({ busca, setBusca }: BuscadorProps) {
+function Buscador({ busca, setBusca }: BuscadorProps) {
     // forma longa de escrever
     // const aoAlterado = (event: React.ChangeEvent<HTMLInputElement>) => {
     //     setBusca(event.target.value)
     //     console.log(event.target.value)
     // }
 
+    const elemento = useMemo(() =>  <CgSearch size={20} color="#4C4D5E" />, []);
+
     return (
         <div className={styles.buscador}>
             {/* <input onChange={aoAlterado} value={busca}/> // forma longa */}
             <input onChange={(event) => setBusca(event.target.value)} value={busca} placeholder="Buscar" />
-            <CgSearch size={20} color="#4C4D5E" />
+            {/* <CgSearch size={20} color="#4C4D5E" /> */}
+            {elemento}
         </div>
     );
 }
+
+export default memo(Buscador);
